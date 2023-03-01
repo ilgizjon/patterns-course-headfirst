c11e581c3223ab144e544ad51751a4e69887f44f
Ilgiz Sharipov <bossilgizj@mail.ru>
2023-03-01 15:19:06 +0300

    Add package Component

---
+++ src/HeadFirst/Component/MenuTestDrive.java
@@ -0,0 +1,36 @@
+package HeadFirst.Component;
+
+public class MenuTestDrive {
+    public static void main(String args[]) {
+        MenuComponent pancakeHouseMenu =
+                new Menu("PANCAKE HOUSE MENU", "Breakfast");
+        MenuComponent dinerMenu =
+                new Menu("DINER MENU", "Lunch");
+        MenuComponent cafeMenu =
+                new Menu("CAFE MENU", "Dinner");
+        MenuComponent dessertMenu =
+                new Menu("DESSERT MENU", "Dessert of course!");
+
+        MenuComponent allMenus = new Menu("ALL MENUS", "All menus combined");
+        allMenus.add(pancakeHouseMenu);
+        allMenus.add(dinerMenu);
+        allMenus.add(cafeMenu);
+        // добавление других элементов
+        dinerMenu.add(new MenuItem(
+                "Pasta",
+                "Spaghetti with Marinara Sauce, and a slice of sourdough bread",
+                true,
+                3.89));
+
+        dinerMenu.add(dessertMenu);
+        dessertMenu.add(new MenuItem(
+                "Apple Pie",
+                "Apple pie with a flakey crust, topped with vanilla icecream",
+                true,
+                1.59));
+        // добавление других элементов
+        Waitress waitress = new Waitress(allMenus);
+        waitress.printMenu();
+    }
+
+}
